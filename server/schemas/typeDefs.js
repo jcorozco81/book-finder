7938f827eae10a084cb682331d3c11cd0e98dc97
jcorozco81 <jcorozco@gmail.com>
2021-10-03 02:44:49 -0400

    Added mutation and queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-
+    _id: ID
     username: String!
     email: String!
     password: String!
@@ -22,9 +22,25 @@ const typeDefs = gql`
   }
 
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     users: [User]
+    user(username: String!): User
+    me: User
   }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    loginUser(email: String!, password: String!): Auth
+  }
+
+
+
+
 `;
 
 module.exports = typeDefs;
