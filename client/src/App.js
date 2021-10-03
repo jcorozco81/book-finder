040f19ccba89f0f2c8e65816b9a82b5be6a2c857
jcorozco81 <jcorozco@gmail.com>
2021-10-03 02:58:28 -0400

    Added apollo client and apollo provider code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+
+// Imported Apollo Client
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Added Apollo Client
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +28,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
